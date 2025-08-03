b9b5a842f4eaf3246a80dcec5ce2ac4cc0e59b77
STefan Mayer <stefan@clumsy.ch>
2025-08-03 12:51:47 +0200

    changed fro system reset to zb_resetDevice for 5x button press

--- src/zigbee/switch_cluster.c
+++ src/zigbee/switch_cluster.c
@@ -341,7 +341,8 @@ void switch_cluster_on_button_multi_press(zigbee_switch_cluster *cluster, u8 pre
   if (press_count > MULTI_PRESS_CNT_TO_RESET)
   {
 //     factoryReset();
-     SYSTEM_RESET();
+//     SYSTEM_RESET();
+     zb_resetDevice();
   }
 }
 
