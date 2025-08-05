29423cd7fc43833ae27e905b9ca1d7820d16133e
STefan Mayer <stefan@clumsy.ch>
2025-08-05 07:21:29 +0200

    change storage from NV_MODULE_ZCL to NV_MODULE_APP

--- src/zigbee/switch_cluster.c
+++ src/zigbee/switch_cluster.c
@@ -363,12 +363,12 @@ void switch_cluster_store_attrs_to_nv(zigbee_switch_cluster *cluster)
   nv_config_buffer.button_long_press_duration = cluster->button->long_press_duration_ms;
   nv_config_buffer.level_move_rate = cluster->level_move->rate;
 
-  nv_flashWriteNew(1, NV_MODULE_ZCL, NV_ITEM_ZCL_SWITCH_CONFIG(cluster->endpoint), sizeof(zigbee_switch_cluster_config), (u8 *)&nv_config_buffer);
+  nv_flashWriteNew(1, NV_MODULE_APP, NV_ITEM_ZCL_SWITCH_CONFIG(cluster->endpoint), sizeof(zigbee_switch_cluster_config), (u8 *)&nv_config_buffer);
 }
 
 void switch_cluster_load_attrs_from_nv(zigbee_switch_cluster *cluster)
 {
-  nv_sts_t st = nv_flashReadNew(1, NV_MODULE_ZCL, NV_ITEM_ZCL_SWITCH_CONFIG(cluster->endpoint), sizeof(zigbee_switch_cluster_config), (u8 *)&nv_config_buffer);
+  nv_sts_t st = nv_flashReadNew(1, NV_MODULE_APP, NV_ITEM_ZCL_SWITCH_CONFIG(cluster->endpoint), sizeof(zigbee_switch_cluster_config), (u8 *)&nv_config_buffer);
 
   if (st != NV_SUCC)
   {
