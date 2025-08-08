9b59e7a032faa3b13a28351c5bc24270098f2771
STefan Mayer <stefan@clumsy.ch>
2025-08-08 22:18:32 +0200

    hopefully the last try..

--- src/device_config/config_parser.c
+++ src/device_config/config_parser.c
@@ -63,8 +63,21 @@ void init_gpio_output(GPIO_PinTypeDef pin);
 
 void onResetClicked(void *_)
 {
+
+
+  // try to leave network
+  nlme_leave_req_t req;
+  ZB_IEEE_ADDR_COPY(req.deviceAddr, g_zbMacPib.extAddress);
+  req.rejoin = 0;
+  req.removeChildren = 0;
+  if(!g_bdbCtx.leaveDoing){
+    if(RET_OK == zb_nlmeLeaveReq(&req)){
+       g_bdbCtx.leaveDoing = 1;
+    }
+  }
+
   // zb_resetDevice(); // reboot
-  tl_bdbReset2FN(); //leave network, start binding process
+  tl_bdbReset2FN(); // clear network config and start binding process
   // zb_factoryReset();  // leave network, reset device and clear NVRam
 }
 
@@ -72,9 +85,9 @@ void onResetMultiClicked(void *_, u8 press_count)
 {
   if (press_count > MULTI_PRESS_CNT_TO_RESET)
   {
-    // tl_bdbReset(); //leave network, start binding process
-    // zb_factoryReset(); // leave network, reset device and clear NVRam
-    reset_to_default_config();
+    // tl_bdbReset2FN(); // clear network config and start binding process
+    // zb_factoryReset(); // reset device and clear NVRam
+    reset_to_default_config(); // clear app config => need to do an interview again afterwards!
     // zb_resetDevice(); // reboot 
   }
 }
