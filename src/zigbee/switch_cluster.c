3792db507367287513ba3f20be5bd02b347473ec
STefan Mayer <stefan@clumsy.ch>
2025-08-08 11:56:22 +0200

    check zb_resetDevice()

--- src/zigbee/switch_cluster.c
+++ src/zigbee/switch_cluster.c
@@ -345,9 +345,10 @@ void switch_cluster_on_button_multi_press(zigbee_switch_cluster *cluster, u8 pre
 {
   if (press_count > MULTI_PRESS_CNT_TO_RESET)
   {
+//    tl_bdbReset2FN();
 //     factoryReset();
-     SYSTEM_RESET();
-//     zb_resetDevice();
+//     SYSTEM_RESET();
+     zb_resetDevice();
   }
 }
 
